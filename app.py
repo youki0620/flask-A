e8903b22261214ffa1b12b730a970e4c32893855
Tanaka Yoki <yangjit41@gmail.com>
2022-12-02 19:09:18 +0900

    ラッキーカラーおみくじまで

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+import sqlite3,random
+
 @app.route("/")
 def helloworld():
     return"<h1>Flaskからハローワールド</h1>"
@@ -23,9 +25,18 @@ def weather():
     py_weather = "はれ"
     return render_template("/weather.html",tenki = py_weather)
 
+# -----------2日目----------------
 
-
-
+@app.route("/color")
+def color():
+    conn = sqlite3.connect("color.db")
+    c = conn.cursor()
+    c.execute("select * from colors;")
+    py_color = c.fetchall()
+    py_color = random.choice(py_color)
+    print(py_color)
+    c.close()
+    return render_template("color.html",iro = py_color)
 
 
 
